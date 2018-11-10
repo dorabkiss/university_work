d9fac42ee736f634997426d949e9f0dc4a315fa4
Dora B Kiss <dkiss001@gold.ac.uk>
2018-11-10 11:22:41 +0000

    delete redundant transform_data method, fix bug in one hot encoding method (reset_index() adter encoding)

--- data_mining_knn/src/preprocess.py
+++ data_mining_knn/src/preprocess.py
@@ -69,114 +69,9 @@ def cleanData(train_dataset, test_dataset):
 
     # adult_deduplicated.drop('capital_gain', axis=1, inplace=True)
     # adult_deduplicated.drop('capital_loss', axis=1, inplace=True)
-
-    # remove less significant columns
-    # fnlwgt
     # adult_deduplicated.drop('fnlwgt', axis=1, inplace=True)
 
     return train_dataset_clean, test_dataset_clean
-
-def transformData(train_dataset, test_dataset):
-    """
-    Rearrange the training and testing data to make them more simple and meaningful 
-    by reducing the number of categories for nominal variables.
-    Args:
-        train_dataset: Train data as Pandas DataFrame.
-        test_dataset: Test data as Pandas DataFrame.
-    Returns:
-        train_dataset: Transformed training data in Pandas DataFrame.
-        test_dataset: Transformed testing data in Pandas DataFrame.
-    """
-    # copy and modify that dataset or just call functions on input dataset?
-    #result_train = train_dataset.copy()
-    #result_test = test_dataset.copy()
-
-    # some data are too detailed, like 'marital-status' and ‘native_country’.
-    # this function modifies the input datasets!
-
-    # where relationship is 'Husband' or 'Wife', change it to 'Married'
-    train_dataset.loc[((train_dataset['relationship'] == 'Husband') | (train_dataset['relationship'] == 'Wife')) , "relationship"] = 'Married'
-    test_dataset.loc[((test_dataset['relationship'] == 'Husband') | (test_dataset['relationship'] == 'Wife')) , "relationship"] = 'Married'
-    
-    # where marital_status is 'Married-*', change it to 'Married'
-    train_dataset.loc[((train_dataset['marital_status'] == 'Married-civ-spouse') | (train_dataset['marital_status'] == 'Married-AF-spouse') | (train_dataset['marital_status'] == 'Married-spouse-absent')) , "marital_status"] = 'Married'
-    test_dataset.loc[((test_dataset['marital_status'] == 'Married-civ-spouse') | (test_dataset['marital_status'] == 'Married-AF-spouse') | (test_dataset['marital_status'] == 'Married-spouse-absent')) , "marital_status"] = 'Married'
-
-    # where marital_status is 'Divorced' or 'Never-married' or 'Separated' or 'Widowed', change it to 'Unmarried'
-    train_dataset.loc[((train_dataset['marital_status'] == 'Divorced') | (train_dataset['marital_status'] == 'Never-married') | (train_dataset['marital_status'] == 'Separated') | (train_dataset['marital_status'] == 'Widowed')) , "marital_status"] = 'Unmarried'
-    test_dataset.loc[((test_dataset['marital_status'] == 'Divorced') | (test_dataset['marital_status'] == 'Never-married') | (test_dataset['marital_status'] == 'Separated') | (test_dataset['marital_status'] == 'Widowed')) , "marital_status"] = 'Unmarried'
-
-    # native_country column: group the countries by global regions
-    # assuming that 'South' stands for South Korea and 'Hong' for Hong Kong
-    train_dataset['native_country'] = train_dataset['native_country'].map({'United-States': 'North_America', 'Mexico': 'North_America', 'Philippines': 'Asia', 'Germany': 'Europe', 'Canada': 'North_America', 'Puerto-Rico': 'Central_America', 'Jamaica': 'Central_America', 'India': 'Asia', 'South': 'Asia', 'China': 'Asia', 'El-Salvador': 'Central_America', 'Dominican-Republic': 'Central_America', 'England': 'Europe', 'Poland': 'Europe', 'Columbia': 'South_America', 'Cuba': 'Central_America', 'Italy': 'Europe', 'Japan': 'Asia', 'Vietnam': 'Asia', 'Guatemala': 'Central_America', 'Iran': 'Asia', 'France': 'Europe', 'Nicaragua': 'Central_America', 'Peru': 'South_America', 'Taiwan': 'Asia', 'Greece': 'Europe', 'Ireland': 'Europe', 'Ecuador': 'South_America', 'Haiti': 'Central_America', 'Cambodia': 'Asia', 'Hong': 'Asia', 'Portugal': 'Europe', 'Trinadad&Tobago': 'Central_America', 'Thailand': "Asia", 'Yugoslavia': 'Europe', 'Honduras': 'Central_America', 'Laos': 'Asia', 'Hungary': 'Europe', 'Scotland': 'Europe', 'Holand-Netherlands': 'Europe', 'Outlying-US(Guam-USVI-etc)': 'North_America'})
-    test_dataset['native_country'] = test_dataset['native_country'].map({'United-States': 'North_America', 'Mexico': 'North_America', 'Philippines': 'Asia', 'Germany': 'Europe', 'Canada': 'North_America', 'Puerto-Rico': 'Central_America', 'Jamaica': 'Central_America', 'India': 'Asia', 'South': 'Asia', 'China': 'Asia', 'El-Salvador': 'Central_America', 'Dominican-Republic': 'Central_America', 'England': 'Europe', 'Poland': 'Europe', 'Columbia': 'South_America', 'Cuba': 'Central_America', 'Italy': 'Europe', 'Japan': 'Asia', 'Vietnam': 'Asia', 'Guatemala': 'Central_America', 'Iran': 'Asia', 'France': 'Europe', 'Nicaragua': 'Central_America', 'Peru': 'South_America', 'Taiwan': 'Asia', 'Greece': 'Europe', 'Ireland': 'Europe', 'Ecuador': 'South_America', 'Haiti': 'Central_America', 'Cambodia': 'Asia', 'Hong': 'Asia', 'Portugal': 'Europe', 'Trinadad&Tobago': 'Central_America', 'Thailand': "Asia", 'Yugoslavia': 'Europe', 'Honduras': 'Central_America', 'Laos': 'Asia', 'Hungary': 'Europe', 'Scotland': 'Europe', 'Holand-Netherlands': 'Europe', 'Outlying-US(Guam-USVI-etc)': 'North_America'})
-
-    #TODO
-    # workclass: to reduce the number of categories, collapse local-gov, state-gov and federal-gov to gov
-    #train_dataset['workclass'] = train_dataset['workclass'].map({'Private': 0, 'Self-emp-not-inc': 1, 'Local-gov': 'gov', 'State-gov': 'gov', 'Self-emp-inc': 4, 'Federal-gov': 'gov', 'Without-pay': 6})
-    
-
-    return train_dataset, test_dataset
-
-def encodeCategoricalData(train_dataset, test_dataset):
-    """
-    Convert categorical data into numeric. KNN cannot work with strings.
-    Args:
-        train_dataset: Train dataset as Pandas DataFrame.
-        test_dataset: Test dataset as Pandas DataFrame.
-    Returns:
-        train_dataset: Cleaned training data in Pandas DataFrame.
-        test_dataset: Cleaned testing data in Pandas DataFrame.
-    """  
-    # workclass volumn: 0 for 'Private', 1 for 'Self-emp-not-inc', 2 for 'Local-gov', 3 for 'State-gov', 4 for 'Self-emp-inc', 5 for 'Federal-gov', 6 for 'Without-pay'  
-    train_dataset['workclass'] = train_dataset['workclass'].map({'Private': 0, 'Self-emp-not-inc': 1, 'Local-gov': 2, 'State-gov': 3, 'Self-emp-inc': 4, 'Federal-gov': 5, 'Without-pay': 6}).astype(int)
-    test_dataset['workclass'] = test_dataset['workclass'].map({'Private': 0, 'Self-emp-not-inc': 1, 'Local-gov': 2, 'State-gov': 3, 'Self-emp-inc': 4, 'Federal-gov': 5, 'Without-pay': 6}).astype(int)
-
-    # marital_status column: 0 for 'Married', 1 for 'Unmarried'
-    train_dataset['marital_status'] = train_dataset['marital_status'].map({'Married': 0, 'Unmarried': 1}).astype(int)
-    test_dataset['marital_status'] = test_dataset['marital_status'].map({'Married': 0, 'Unmarried': 1}).astype(int)
-    
-    # TODO occupation
-
-    # relationship: 0 for 'Married', 1 for 'Unmarried', 2 for 'Not-in-family', 3 for 'Own-child', 4 for 'Other-relative'
-    train_dataset['relationship'] = train_dataset['relationship'].map({'Married': 0, 'Unmarried': 1, 'Not-in-family': 2, 'Own-child': 3, 'Other-relative': 4}).astype(int)
-    test_dataset['relationship'] = test_dataset['relationship'].map({'Married': 0, 'Unmarried': 1, 'Not-in-family': 2, 'Own-child': 3, 'Other-relative': 4}).astype(int)
-
-    # race column: 0 for 'White', 1 for 'Black', 2 for 'Asian-Pac-Islander', 3 for 'Amer-Indian-Eskimo', 4 for 'Other'
-    train_dataset['race'] = train_dataset['race'].map({'White': 0, 'Black': 1, 'Asian-Pac-Islander': 2, 'Amer-Indian-Eskimo': 3, 'Other': 4}).astype(int)
-    test_dataset['race'] = test_dataset['race'].map({'White': 0, 'Black': 1, 'Asian-Pac-Islander': 2, 'Amer-Indian-Eskimo': 3, 'Other': 4}).astype(int)
-
-    # sex column: 0 for 'Female', 1 for 'Male'
-    train_dataset['sex'] = train_dataset['sex'].map({'Female': 0, 'Male': 1}).astype(int)
-    test_dataset['sex'] = test_dataset['sex'].map({'Female': 0, 'Male': 1}).astype(int)
-    
-    # native_country: 0 for 'North_America', 1 for 'Asia', 2 for 'Central_America', 3 for 'Europe', 4 for 'South_America'
-    train_dataset['native_country'] = train_dataset['native_country'].map({'North_America': 0, 'Asia': 1, 'Central_America': 2, 'Europe': 3, 'South_America': 4}).astype(int)
-    test_dataset['native_country'] = test_dataset['native_country'].map({'North_America': 0, 'Asia': 1, 'Central_America': 2, 'Europe': 3, 'South_America': 4}).astype(int)
-
-    # earns column: 0 for '<=50K', 1 for '>50K'
-    train_dataset['earns'] = train_dataset['earns'].map({'<=50K': 0, '>50K': 1}).astype(int)
-    test_dataset['earns'] = test_dataset['earns'].map({'<=50K': 0, '>50K': 1}).astype(int)
-
-    return train_dataset, test_dataset
-
-def encodeCategoricalData2(train_ds):
-    """
-    Convert categorical data into numeric. KNN cannot work with strings.
-    Args:
-        train_dataset: Train dataset as Pandas DataFrame.
-        test_dataset: Test dataset as Pandas DataFrame.
-    Returns:
-        train_dataset: Cleaned training data in Pandas DataFrame.
-        test_dataset: Cleaned testing data in Pandas DataFrame.
-    """     
-    # convert 'object' columns to a 'category', then use those category values for label encoding.
-    # Then assign the encoded variable to a new column using the cat.codes accessor:
-    for column in train_ds.columns:
-        if train_ds.dtypes[column] == np.object:
-            train_ds[column] = train_ds[column].astype('category')
-            train_ds[column] = train_ds[column].cat.codes
-    return train_ds
 
 def normalize(df):
     """
@@ -227,10 +122,11 @@ def ohe_data(X_train, y_train, X_test, y_test):
     data_ohe = pd.get_dummies(data)
     X_train_ohe = data_ohe[:len(X_train)]
     X_test_ohe = data_ohe[len(X_train):]
+    X_train_ohe.reset_index(drop=True)
 
     # earns column: 0 for '<=50K', 1 for '>50K'
-    y_train_ohe = y_train.map({'<=50K': 0, '>50K': 1}).astype(int)
-    y_test_ohe = y_test.map({'<=50K': 0, '>50K': 1}).astype(int)
+    y_train_ohe = y_train.map({'<=50K': 0, '>50K': 1}).astype(int).reset_index(drop=True)
+    y_test_ohe = y_test.map({'<=50K': 0, '>50K': 1}).astype(int).reset_index(drop=True)
     # or we can use replace instead of map
     # y_train_ohe = y_train.replace([' <=50K', ' >50K'], [0, 1])
     # y_test_ohe = y_test.replace([' <=50K', ' >50K'], [0, 1])
